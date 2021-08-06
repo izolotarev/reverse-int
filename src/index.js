03142cbc0f4d5acd54ad5b3342b7d6e8088aca80
Igor Zolotarev <izolotarev@hotmail.com>
2021-08-06 18:56:53 +0300

    feat: add solution to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n * -1;
+    }
+
+    let reversed = 0;
+
+    while (n) {
+        reversed = reversed * 10 + (n % 10);
+        n = Math.floor(n / 10);
+    }
+    return reversed;
+};
